6b9b788ea6af252ef8464ad22b9cdea59c0ce7cc
mdMandoinfo <mdMandoinfo@gmail.com>
2022-05-10 04:09:01 +0200

    Create main.js

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+let title = document.querySelector('.tittle');
+let ul = document.querySelector('ul');
+let reload = document.querySelector('.reload');
+
+window.onload = function(){
+    if(window.navigator.onLine){
+        onLine()
+    }else{
+        offLine()
+    }
+}
+
+window.addEventListener("online", function(){
+   onLine(); 
+});
+window.addEventListener("offline", function(){
+    offLine(); 
+});
+
+reload.onclick = function(){
+    window.location.reload();
+}
+function onLine(){
+    title.innerHTML = 'Online Now';
+    title.style.color = 'green';
+    ul.classList.add('hide');
+    reload.classList.add('hide');
+}
+function offLine(){
+    title.innerHTML = 'Error No Internet';
+    title.style.color = '#666';
+    ul.classList.remove('hide');
+    reload.classList.remove('hide');
+}
